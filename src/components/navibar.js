892daea9f947fac0b10f0812100607dee2b7bd50
JordanMSchall <jordan.m.schall@gmail.com>
2021-02-02 17:04:12 -0600

    Added some navigation functionality.

--- src/components/navibar.js
+++ src/components/navibar.js
@@ -12,24 +12,28 @@ class NaviBar extends React.Component {
     return (
       <div className="navbar">
         <Navbar bg="light" expand="lg" variant="light">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">SCHALL.io</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <Nav.Link href="#link">Game</Nav.Link>
+              <NavDropdown title="Social Media" id="basic-nav-dropdown">
+                <NavDropdown.Item href="https://www.linkedin.com/in/jordan-schall" target="_blank">Linkedin</NavDropdown.Item>
+                <NavDropdown.Item href="https://medium.com/@jordan.m.schall" target="_blank">Medium</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.facebook.com/profile.php?id=100022276723485" target="_blank">Facebook</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.instagram.com/schall.io/" target="_blank">Instagram</NavDropdown.Item>
+                <NavDropdown.Item href="https://twitter.com/phuzzii" target="_blank">Twitter</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="API's">
+                <NavDropdown.Item href="#action/3.4">phuzzii-API</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
             </Nav>
-            <Form inline>
+            {/* <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
               <Button variant="outline-success">Search</Button>
-            </Form>
+            </Form> */}
           </Navbar.Collapse>
         </Navbar>
       </div>
