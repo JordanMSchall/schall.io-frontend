85bc852bcc2a546ca2f6701bbbcfbd1d136c6ccd
JordanMSchall <jordan.m.schall@gmail.com>
2021-02-02 19:16:10 -0600

    Fixed navbar for phones.

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../css/footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        Footer Content
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import NaviBar from './components/navibar';
 import WelcomeContent from './components/welcomecontent';
+import Footer from './components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
@@ -14,11 +15,16 @@ class App extends React.Component {
       return <WelcomeContent/>;
   }
 
+  renderFooter() {
+      return <Footer/>;
+  }
+
   render() {
     return (  
       <div id="app">
         {this.renderNavBar()}
         {this.renderWelcomeContent()}
+        {this.renderFooter()}
       </div>
     );
   }
